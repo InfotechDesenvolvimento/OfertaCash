f519de2f480f13ff6073410b349d5b67832f9ade
Gabriel Lucas Figueiro <desenvolvimento@infotech-solucoes.com>
2021-07-15 17:04:18 -0300

    Atualização no dashboard

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,13 +4,14 @@
 
 <link rel="stylesheet" type="text/css" href="{{ asset('resources/css/home.css') }}">
 <link rel="stylesheet" type="text/css" href="https://getbootstrap.com/docs/4.0/examples/sign-in/signin.css">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
 @section('content')
     
                     
-    <form class="form-signin" method="POST" action="{{ url('/login') }}" style="margin-top: 70px;">
+    <form class="form-signin w3-animate-opacity" method="POST" action="{{ url('/login') }}" style="margin-top: 70px;">
 
-        <img class="mb-4 mx-auto d-block" src="{{ asset('resources/images/logo_ofertacash.png') }}" width="150">
+        <img class="mb-4 mx-auto d-block" src="{{ asset('resources/images/logo_ofertacash.png') }}" width="150" style="padding-left: 4px">
         <h3 class="text-center mb-3">Login</h3>
         @if(isset($msg))
             <div class="alert alert-primary" role="alert">
@@ -20,12 +21,9 @@
         <input type ="hidden" name="_token" value="{{{ csrf_token() }}}">
         <input type="text" id="usuario" name="usuario" class="form-control" placeholder="Usuário" required autofocus>
         <input type="password" id="senha" name="senha" class="form-control" placeholder="Senha" required>
-        <div class="checkbox mb-3">
-            <label>
-                <input type="checkbox" value="remember-me"> Lembrar-me
-            </label>
+        <div class="d-grid gap-2 mt-4">
+            <button class="btn btn-primary btn-block" type="submit">Entrar</button>
         </div>
-        <button class="btn btn-primary btn-block" type="submit">Entrar</button>
     </form>
 
     <script>
--- resources/views/cliente/home.blade.php
+++ resources/views/cliente/home.blade.php
@@ -2,25 +2,57 @@
 
 <link rel="stylesheet" href="{{ asset('resources/css/cliente/home.css') }}">
 
-@section('conteudo')
-    <div class="fundo">
-        <div class="box">
-            <div class="title">
-                <h3>Saldo Disponível</h3>
-                <hr>
+@section('content')
+    <h2 id="dashboard" style="margin-top: 80px;">Dashboard</h2>
+    <hr>
+
+<!--     <div class="container shadow-lg dashboard rounded mt-5" id="container">
+        <div class="row" style="padding: 10px;">
+            <div class="col-2 simbolo rounded-circle" style="background-color: #02CA95;">
+                <i class="fas fa-dollar-sign fa-3x simbolo-fa"></i>
             </div>
-            <div class="title">
-                <h4> <i class="fas fa-dollar-sign"></i> {{ Auth::user()->SALDO_ATUAL }}</h4>
+            <div class="col-4" id="card01">
+                <h3 class="subtitulo-dashboard">Saldo Disponível</h3>
+                <h2 class="valor-dashboard" id="saldo-atual">$ {{ Auth::user()->SALDO_ATUAL }}</h2>
             </div>
         </div>
-        <br><br>
-        <div class="box">
-            <div class="title">
-                <h3>Saldo Payback</h3>
-                <hr>
+    </div>
+
+    <div class="container shadow-lg dashboard rounded" id="container">
+        <div class="row" style="padding: 10px;">
+            <div class="col-2 simbolo rounded-circle card02" style="background-color: #9877DC;">
+                <i class="fas fa-hand-holding-usd fa-3x simbolo-fa"></i>
             </div>
-            <div class="title">
-                <h4><i class="fas fa-dollar-sign"></i> {{ Auth::user()->saldo_payback }}</h4>
+            <div class="col-4 card03">
+                <h3 class="subtitulo-dashboard">Saldo Payback</h3>
+                <h2 class="valor-dashboard" id="saldo-payback">$ {{ Auth::user()->saldo_payback }}</h2>
+            </div>
+        </div>
+    </div> -->
+
+    <div class="d-flex">
+
+        <div class="container shadow-lg dashboard rounded m-3 flex-wrap">
+            <div class="row" style="padding: 10px;">
+                <div class="col-6 simbolo rounded-circle" style="background-color: #02CA95;">
+                    <i class="fas fa-dollar-sign fa-3x simbolo-fa"></i>
+                </div>
+                <div class="col-6" id="card01">
+                    <h3 class="subtitulo-dashboard">Saldo Disponível</h3>
+                    <h2 class="valor-dashboard" id="saldo-atual">$ {{ Auth::user()->SALDO_ATUAL }}</h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="container shadow-lg dashboard rounded m-3">
+            <div class="row" style="padding: 10px;">
+                <div class="col-6 simbolo rounded-circle card02" style="background-color: #9877DC;">
+                    <i class="fas fa-hand-holding-usd fa-3x simbolo-fa"></i>
+                </div>
+                <div class="col-6 card03">
+                    <h3 class="subtitulo-dashboard">Saldo Payback</h3>
+                    <h2 class="valor-dashboard" id="saldo-payback">$ {{ Auth::user()->saldo_payback }}</h2>
+                </div>
             </div>
         </div>
     </div>
